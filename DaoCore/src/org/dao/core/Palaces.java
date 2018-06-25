55cc081b9b6e8ac9572ca23d36183ae0d7ba069a
weihsin2002 <35787286+weihsin2002@users.noreply.github.com>
2018-06-24 21:40:18 -0700

    Added Earthly Branches to Palaces

--- DaoCore/src/org/dao/core/Palaces.java
+++ DaoCore/src/org/dao/core/Palaces.java
@@ -1,24 +1,28 @@
 package org.dao.core;
 
 public enum Palaces {
-	KAN (1, FiveElements.WATER, "Kan"),
-	GEN (8, FiveElements.EARTH, "Gen"),
-	ZHEN (3, FiveElements.WOOD, "Zhen"),
-	XUN (4, FiveElements.WOOD, "Xun"),
-	LI (9, FiveElements.FIRE, "Li"),
-	KUN (2, FiveElements.EARTH, "Kun"),
-	DUI (7, FiveElements.METAL, "Dui"),
-	QIAN (6, FiveElements.METAL, "Qian"),
-	ZHONG (5, FiveElements.EARTH, "Zhong");
+	KAN (1, FiveElements.WATER, "Kan", EarthlyBranches.ZI, EarthlyBranches.CHOU),
+	GEN (8, FiveElements.EARTH, "Gen", EarthlyBranches.YIN, null),
+	ZHEN (3, FiveElements.WOOD, "Zhen", EarthlyBranches.MOU, EarthlyBranches.CHEN),
+	XUN (4, FiveElements.WOOD, "Xun", EarthlyBranches.SI, null),
+	LI (9, FiveElements.FIRE, "Li", EarthlyBranches.WU, EarthlyBranches.WEI),
+	KUN (2, FiveElements.EARTH, "Kun", EarthlyBranches.SHENG, null),
+	DUI (7, FiveElements.METAL, "Dui", EarthlyBranches.YOU, EarthlyBranches.XU),
+	QIAN (6, FiveElements.METAL, "Qian", EarthlyBranches.HAI, null),
+	ZHONG (5, FiveElements.EARTH, "Zhong", null, null);
 	
 	int order;
 	FiveElements fiveElements;
 	String gong;
+	EarthlyBranches earthlyBranch1;
+	EarthlyBranches earthlyBranch2;
 	
-	Palaces (int order, FiveElements fiveElements, String gong) {
+	Palaces (int order, FiveElements fiveElements, String gong, EarthlyBranches earthlyBranch1, EarthlyBranches earthlyBranch2) {
 		this.order = order;
 		this.fiveElements = fiveElements;
 		this.gong = gong;
+		this.earthlyBranch1 = earthlyBranch1;
+		this.earthlyBranch2 = earthlyBranch2;
 	}
 
 	public int order() {
